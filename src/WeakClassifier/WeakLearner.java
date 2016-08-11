2560b4ab54678a6c8016840525554b8e11c63429
AcharyaAditya <fabritya@gmail.com>
2016-08-11 18:13:41 +0545

    Weak Classifier Additions

--- src/WeakClassifier/WeakLearner.java
+++ src/WeakClassifier/WeakLearner.java
@@ -6,26 +6,36 @@
 package WeakClassifier;
 
 import WeakClassifier.util.CummulativeSummation;
+import WeakClassifier.util.SortPlusIndex;
 
 /**
  *
- * @author user
+ * @author Aditya Acharya
  */
 public class WeakLearner {
 
     public void TrainWeakLearner(int posImageNum, int negImageNumber, int totalImage, float[] weights, int[] labels, float[][] completeFeatures) {
         float tPlus = 0, tMinus = 0;
-        float[] oneFeature = new float[113472];
-        float[] sortedOneFeature = new float[113472];
+        int polar, polarity, featInd;
+        float minErr = 999999999;
+        float threshold;
+
+        int[] result = new int[totalImage];
+        int[] bestResult = new int[totalImage];
+        float[] oneFeat = new float[totalImage];
+        float[] sortOneFeat = new float[totalImage];
         float[] cumSortedOneFeature = new float[113472];
+
+        Integer[] sortInd = new Integer[totalImage];
+        float[] sortWeights = new float[totalImage];
+        int[] sortLabels = new int[totalImage];
         float[] sPlus = new float[totalImage];
         float[] sMinus = new float[totalImage];
         float[] errPlus = new float[totalImage];
         float[] errMinus = new float[totalImage];
+        float[] oneFeatErr = new float[totalImage];
 
-        AscendingBubbleSorting ascendingBubbleSorting = new AscendingBubbleSorting();
-        CummulativeSummation cummulativeSummation = new CummulativeSummation();
-
+        //Total image weights of postitive and negative images
         for (int i = 0; i < posImageNum; i++) {
             tPlus += weights[i];
         }
@@ -33,14 +43,91 @@ public class WeakLearner {
             tMinus += weights[i];
         }
 
+        //For all features of all images
         for (int feaCount = 0; feaCount < 113472; feaCount++) {
 
+            //Get required Feature from all images
             for (int imageCount = 0; imageCount < totalImage; imageCount++) {
-                oneFeature[imageCount] = completeFeatures[imageCount][feaCount];
-                //sort index needed
+                oneFeat[imageCount] = completeFeatures[imageCount][feaCount];
             }
-            sortedOneFeature = ascendingBubbleSorting.Sort(oneFeature);
-            cumSortedOneFeature = cummulativeSummation.CumSum(oneFeature, totalImage);
+
+            //Sorting features and extract pre sorting indices 
+            SortPlusIndex sortPlusIndex = new SortPlusIndex(oneFeat);
+            sortOneFeat = sortPlusIndex.getSortedArray();
+            sortInd = sortPlusIndex.getActualIndex();
+
+            //Sort image weights and labels according to indices extracted from above
+            for (int imageCount = 0; imageCount < totalImage; imageCount++) {
+                sortWeights[imageCount] = weights[sortInd[imageCount]];
+                sortLabels[imageCount] = labels[sortInd[imageCount]];
+            }
+
+            //Cummulative sum of positive and negative image weights after sorting
+            CummulativeSummation cummulativeSummation = new CummulativeSummation(sortWeights, sortLabels, totalImage);
+            sPlus = cummulativeSummation.getSPlus();
+            sMinus = cummulativeSummation.getSMinus();
+
+            /**
+             * Calculate misclassifications and errors according to cumulative
+             * value such that it represents the error of that particular
+             * position value was taken as the classification threshold.
+             */
+            for (int imageCount = 0; imageCount < totalImage; imageCount++) {
+                errPlus[imageCount] = sPlus[imageCount] + (tMinus - sMinus[imageCount]);
+                errMinus[imageCount] = sMinus[imageCount] + (tPlus - sMinus[imageCount]);
+                if (errPlus[imageCount] < errMinus[imageCount]) {
+                    oneFeatErr[imageCount] = errPlus[imageCount];
+                } else {
+                    oneFeatErr[imageCount] = errMinus[imageCount];
+                }
+            }
+
+            //Among all the error values narrow it down to a single minimum error value
+            float minOneFeatErr = oneFeatErr[0];
+            int minInd = 0;
+            for (int imageCount = 1; imageCount < totalImage; imageCount++) {
+                if (oneFeatErr[imageCount] < minOneFeatErr) {
+                    minOneFeatErr = oneFeatErr[imageCount];
+                    minInd = imageCount;
+                }
+            }
+
+            //Choose polarity and result value for the above choosen min error value
+            if (errPlus[minInd] <= errMinus[minInd]) {
+                polar = -1;
+                for (int i = minInd; i < totalImage; i++) {
+                    result[i] = 1;
+                }
+                for (int j = 0; j < totalImage; j++) {
+                    result[sortInd[j]] = result[j];
+                }
+            } else {
+                polar = 1;
+                for (int i = 0; i < minInd; i++) {
+                    result[i] = 1;
+                }
+                for (int j = 0; j < totalImage; j++) {
+                    result[sortInd[j]] = result[j];
+                }
+            }
+
+            //Defining weak clasifier parameters
+            if (minOneFeatErr < minErr) {
+                minErr = minOneFeatErr;
+                if (minInd == 0) {
+                    threshold = (float) (sortOneFeat[0] - 0.5);
+                } else if (minInd == (totalImage - 1)) {
+                    threshold = (float) (sortOneFeat[totalImage - 1] + 0.5);
+                } else {
+                    threshold = (sortOneFeat[minInd - 2] + sortOneFeat[minInd - 1]) / 2;
+                }
+                polarity = polar;
+                featInd = feaCount;
+                for (int i = 0; i < totalImage; i++) {
+                    bestResult[i] = result[i];
+                }
+            }
+
         }
     }
 }
