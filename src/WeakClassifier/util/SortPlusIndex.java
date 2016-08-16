3ac7357ef76f7e0f78ef65ccc68faa955fc32ec0
AcharyaAditya <fabritya@gmail.com>
2016-08-16 10:55:40 +0545

    Function for Integer to int conversion

--- src/WeakClassifier/util/SortPlusIndex.java
+++ src/WeakClassifier/util/SortPlusIndex.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 public class SortPlusIndex {
 
     private Integer[] indexes;
+    private int[] intIndexes;
     private float[] sortedArray;
 
     public SortPlusIndex(float[] featureValue) {
@@ -23,13 +24,22 @@ public class SortPlusIndex {
         indexes = comparator.createIndexArray();
         Arrays.sort(indexes, comparator);
         Arrays.sort(sortedArray);
+        intIndexes = ConvertIntegerToInt(indexes);
     }
 
     public float[] getSortedArray() {
         return this.sortedArray;
     }
 
-    public Integer[] getActualIndex() {
-        return this.indexes;
+    public int[] getActualIndex() {
+        return this.intIndexes;
+    }
+    
+    private int [] ConvertIntegerToInt (Integer [] actualIndex){
+        int[] toInt = new int[actualIndex.length];
+        for(int i = 0; i < actualIndex.length; i++){
+            toInt[i] = actualIndex[i];
+        }
+        return toInt;
     }
 }
