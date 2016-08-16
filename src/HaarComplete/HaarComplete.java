78f9add0659ddc53b769dc120e9bcd3b7584dad2
AcharyaAditya <fabritya@gmail.com>
2016-08-16 10:52:24 +0545

    Code cleanup

--- src/HaarComplete/HaarComplete.java
+++ src/HaarComplete/HaarComplete.java
@@ -8,6 +8,7 @@ package HaarComplete;
 import HistogramEqualization.impl.HistEq;
 import HistogramEqualization.util.HistoNormalize;
 import Parallel.ParallelRun;
+import WeakClassifier.WeakLearner;
 import haarfilterall.features.Features;
 import haarfilterall.util.IntegralImage;
 import java.awt.image.BufferedImage;
@@ -36,8 +37,8 @@ public class HaarComplete {
         int[][] intValues = new int[24][24];
         int[] iarray = new int[3];
 
-        int testImagesNum = 179;
-        int totalImageNumber = 180;
+        int testImagesNum = 999;
+        int totalImageNumber = testImagesNum + 1;
         int totalFeaturesNum = 113472;
 
         Features features = new Features();
@@ -47,8 +48,11 @@ public class HaarComplete {
 
         for (int imageCount = 0; imageCount <= testImagesNum; imageCount++) {
             BufferedImage img = null;
-            img = ImageIO.read(new File("G:\\android\\GIT\\CompleteHaar\\image\\test" + (imageCount + 1) + ".png"));
-
+            if (imageCount < 500) {
+                img = ImageIO.read(new File("C:\\Users\\user\\Desktop\\face1\\test" + (imageCount + 1) + ".png"));
+            } else {
+                img = ImageIO.read(new File("C:\\Users\\user\\Desktop\\face1\\test" + (imageCount + 1 + 600) + ".png"));
+            }
             HistEq histEq = new HistEq();                                           //call histogram equilization
             img = histEq.Change(img);                                               //retreive equilized image
 
@@ -78,32 +82,46 @@ public class HaarComplete {
                     //R3.start();
                     //ParallelRun R4 = new ParallelRun("Filter2", 4, intValues);
                     //R4.start();
-                    if (count == 1) {
-                        twoHoriFea = features.FeatureA(2, 1, intValues);
-                        twoVertFea = features.FeatureB(1, 2, intValues);
-                        threeHoriFea = features.FeatureC(3, 1, intValues);
-                        fourRectFea = features.FeatureE(2, 2, intValues);
+//                    if (count == 1) {
+                    twoHoriFea = features.FeatureA(2, 1, intValues);
+                    twoVertFea = features.FeatureB(1, 2, intValues);
+                    threeHoriFea = features.FeatureC(3, 1, intValues);
+                    fourRectFea = features.FeatureE(2, 2, intValues);
 
-                        for (int comboCntr = 0; comboCntr < 36432; comboCntr++) {
-                            completeFeatures[imageCount][comboCntr] = twoHoriFea[comboCntr];
-                        }
-                        for (int comboCntr = 36432; comboCntr < 72864; comboCntr++) {
-                            completeFeatures[imageCount][comboCntr] = twoVertFea[comboCntr - 36432];
-                        }
-                        for (int comboCntr = 72864; comboCntr < 96048; comboCntr++) {
-                            completeFeatures[imageCount][comboCntr] = threeHoriFea[comboCntr - 72864];
-                        }
-                        for (int comboCntr = 96048; comboCntr < 113472; comboCntr++) {
-                            completeFeatures[imageCount][comboCntr] = fourRectFea[comboCntr - 96048];
-                        }
+                    for (int comboCntr = 0; comboCntr < 36432; comboCntr++) {
+                        completeFeatures[imageCount][comboCntr] = twoHoriFea[comboCntr];
                     }
+                    for (int comboCntr = 36432; comboCntr < 72864; comboCntr++) {
+                        completeFeatures[imageCount][comboCntr] = twoVertFea[comboCntr - 36432];
+                    }
+                    for (int comboCntr = 72864; comboCntr < 96048; comboCntr++) {
+                        completeFeatures[imageCount][comboCntr] = threeHoriFea[comboCntr - 72864];
+                    }
+                    for (int comboCntr = 96048; comboCntr < 113472; comboCntr++) {
+                        completeFeatures[imageCount][comboCntr] = fourRectFea[comboCntr - 96048];
+                    }
+//                    }
                 }
                 //Normalize Histogram Portion Remaining!!
             }
 
         }
-
-//        System.out.println("IMAGE NUMBER = " + count);
+        float[] weights = new float[totalImageNumber];
+        int[] labels = new int[totalImageNumber];
+        for (int c = 0; c < 500; c++) {
+            weights[c] = (float) 500 / (2 * 500);
+            labels[c] = 1;
+        }
+        for (int c = 500; c < 1000; c++) {
+            weights[c] = (float) 500 / (2 * 500);
+            labels[c] = 0;
+        }
+        WeakLearner weakLearner = new WeakLearner();
+        weakLearner.TrainWeakLearner(500, 500, totalImageNumber, weights, labels, completeFeatures);
+        System.out.println("Threshold " + weakLearner.getThreshold());
+        System.out.println("Fea INDEX " + weakLearner.getFeatInd());
+        System.out.println("Polarity " + weakLearner.getPolarity());
+        System.out.println("Min ERROR " + weakLearner.getMinErr());
     }
 
 }
